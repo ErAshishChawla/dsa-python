2bba76a3074a5a0c71384ef7e83a7beb6e20c5d8
ErAshishChawla <ashishchawladev@gmail.com>
2024-02-28 22:36:04 -0500

    completed print_statement

---
+++ 1. Basics/print_statement.py
@@ -0,0 +1,9 @@
+print("hello world")
+print("Good bye")
+print("We are learning python")
+
+print("----------")
+
+print("hello world", end=" ")
+print("good bye", end=" ")
+print("we are learning python")
