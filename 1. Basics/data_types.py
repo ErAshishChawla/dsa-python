acad94552d2b344e2cca836b0ac6b9753bf50b20
ErAshishChawla <ashishchawladev@gmail.com>
2024-03-02 15:38:43 -0500

    Basic Types completed

---
+++ 1. Basics/data_types.py
@@ -0,0 +1,17 @@
+"""
+Data Types in Python
+1. Integer(int) -> 5,6,0,-99, 66
+2. Float(float) ->  55.56, 0.0, -99.99
+3. Complex Number -> Not needed as such
+
+4. String(str) -> "Hello", "World", "Python", "3.7" (Anything in double quotes or single quotes)
+There is no char in python, single character is also a string
+
+
+"""
+
+a = 5
+print(type(a), a)
+
+b = -0.0
+print(type(b), b)
